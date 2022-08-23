047a9a53da129959e22bd251c730264a2d0d6489
Tekesh10 <gtcsantekesh@gmail.com>
2022-08-23 08:24:28 +0530

    Added: Size method in Binary Tree

--- src/com/bridgelabz/BinarySearchTree/MyBinaryTree.java
+++ src/com/bridgelabz/BinarySearchTree/MyBinaryTree.java
@@ -15,11 +15,28 @@ public class MyBinaryTree<K extends Comparable<K>> {
             currentNode.left = addRecursively(currentNode.left, key);
         return currentNode;
     }
-
+    public void getSize() {
+        int size = getSizeRecursively(root);
+        if (size == 0) {
+            System.out.println("Tree is empty");
+        } else {
+            System.out.println("Elements in tree = " + size);
+        }
+    }
+    private int getSizeRecursively(MyBinaryNode<K> root) {
+        int size;
+        if (root == null) {
+            size = 0;
+        }else {
+            size = this.getSizeRecursively(root.left) + 1 + this.getSizeRecursively(root.right);
+        }
+        return size;
+    }
     public static void main(String[] args) {
         MyBinaryTree<Integer> tree = new MyBinaryTree<>();
         tree.add(56);
         tree.add(30);
         tree.add(70);
+        tree.getSize();
     }
 }
