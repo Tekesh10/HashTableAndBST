b95e8176c2dcb905c08c80fcb9743d84ca6ead93
Tekesh10 <gtcsantekesh@gmail.com>
2022-08-23 08:35:10 +0530

    Added: Ability to search in Binary Tree

--- src/com/bridgelabz/BinarySearchTree/MyBinaryTree.java
+++ src/com/bridgelabz/BinarySearchTree/MyBinaryTree.java
@@ -1,10 +1,14 @@
 package com.bridgelabz.BinarySearchTree;
 
+import java.util.Scanner;
+
 public class MyBinaryTree<K extends Comparable<K>> {
     MyBinaryNode<K> root;
+
     public void add(K key) {
         this.root = this.addRecursively(root, key);
     }
+
     MyBinaryNode<K> addRecursively(MyBinaryNode<K> currentNode, K key) {
         if (currentNode == null)
             return new MyBinaryNode<>(key);
@@ -15,6 +19,7 @@ public class MyBinaryTree<K extends Comparable<K>> {
             currentNode.left = addRecursively(currentNode.left, key);
         return currentNode;
     }
+
     public void getSize() {
         int size = getSizeRecursively(root);
         if (size == 0) {
@@ -23,20 +28,49 @@ public class MyBinaryTree<K extends Comparable<K>> {
             System.out.println("Elements in tree = " + size);
         }
     }
+
     private int getSizeRecursively(MyBinaryNode<K> root) {
         int size;
         if (root == null) {
             size = 0;
-        }else {
+        } else {
             size = this.getSizeRecursively(root.left) + 1 + this.getSizeRecursively(root.right);
         }
         return size;
     }
+    public void searchElement(K key) {
+        MyBinaryNode<K> currentNode = this.searchRecursively(root, key);
+        System.out.println(currentNode.key + " FOUND");
+    }
+
+    private MyBinaryNode<K> searchRecursively(MyBinaryNode<K> currentNode, K key) {
+        if (currentNode == null || currentNode.key == key) {
+            return currentNode;
+        }
+        if (key.compareTo(currentNode.key) < 0) {
+            return searchRecursively(currentNode.left, key);
+        }
+        if (key.compareTo(currentNode.key) > 0) {
+            return searchRecursively(currentNode.right, key);
+        }
+        return currentNode;
+    }
+    
     public static void main(String[] args) {
         MyBinaryTree<Integer> tree = new MyBinaryTree<>();
-        tree.add(56);
-        tree.add(30);
-        tree.add(70);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter element: ");
+            int input = scanner.nextInt();
+            if (input == 0) {
+                break;
+            }
+            tree.add(input);
+        }
         tree.getSize();
+        System.out.println("Enter data to search");
+        int searchData = scanner.nextInt();
+        tree.searchElement(searchData);
     }
 }
